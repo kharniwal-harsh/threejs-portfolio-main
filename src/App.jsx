f0c15b09504cc178d0d2801ca189c8f06add5c30
HARSH KHARNIWAL <115343128+kharniwal-harsh@users.noreply.github.com>
2024-11-24 05:49:21 +0000

    modified:   src/App.jsx 	modified:   vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <main className="max-w-7xl mx-auto relative">
       <Navbar />
-      { /*<Hero /> */}
+      <Hero />
       <About />
       <Projects />
       {/* <Clients /> */}
