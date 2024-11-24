d0578db1c2cd1130d444d02eb02a9ef71dd5d84d
harsh kharniwal <115343128+kharniwal-harsh@users.noreply.github.com>
2024-11-24 17:58:55 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,34 +63,34 @@ export const myProjects = [
     subdesc:
       'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
     href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-    texture: '/textures/project/project1.mp4',
-    logo: '/assets/project-logo1.png',
+    texture: './textures/project/project1.mp4',
+    logo: './assets/project-logo1.png',
     logoStyle: {
       backgroundColor: '#2A1816',
       border: '0.2px solid #36201D',
       boxShadow: '0px 0px 60px 0px #AA3C304D',
     },
-    spotlight: '/assets/spotlight1.png',
+    spotlight: './assets/spotlight1.png',
     tags: [
       {
         id: 1,
         name: 'React.js',
-        path: '/assets/react.svg',
+        path: './assets/react.svg',
       },
       {
         id: 2,
         name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        path: './assets/tailwindcss.png',
       },
       {
         id: 3,
         name: 'TypeScript',
-        path: '/assets/typescript.png',
+        path: './assets/typescript.png',
       },
       {
         id: 4,
         name: 'Framer Motion',
-        path: '/assets/framer.png',
+        path: './assets/framer.png',
       },
     ],
   },
@@ -100,34 +100,34 @@ export const myProjects = [
     subdesc:
       'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
     href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
-    texture: '/textures/project/project2.mp4',
-    logo: '/assets/project-logo2.png',
+    texture: './textures/project/project2.mp4',
+    logo: './assets/project-logo2.png',
     logoStyle: {
       backgroundColor: '#13202F',
       border: '0.2px solid #17293E',
       boxShadow: '0px 0px 60px 0px #2F6DB54D',
     },
-    spotlight: '/assets/spotlight2.png',
+    spotlight: './assets/spotlight2.png',
     tags: [
       {
         id: 1,
         name: 'React.js',
-        path: '/assets/react.svg',
+        path: './assets/react.svg',
       },
       {
         id: 2,
         name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        path: './assets/tailwindcss.png',
       },
       {
         id: 3,
         name: 'TypeScript',
-        path: '/assets/typescript.png',
+        path: './assets/typescript.png',
       },
       {
         id: 4,
         name: 'Framer Motion',
-        path: '/assets/framer.png',
+        path: './assets/framer.png',
       },
     ],
   },
@@ -137,8 +137,8 @@ export const myProjects = [
     subdesc:
       'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
     href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
-    texture: '/textures/project/project3.mp4',
-    logo: '/assets/project-logo3.png',
+    texture: './textures/project/project3.mp4',
+    logo: './assets/project-logo3.png',
     logoStyle: {
       backgroundColor: '#60f5a1',
       background:
@@ -146,27 +146,27 @@ export const myProjects = [
       border: '0.2px solid rgba(208, 213, 221, 1)',
       boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
     },
-    spotlight: '/assets/spotlight3.png',
+    spotlight: './assets/spotlight3.png',
     tags: [
       {
         id: 1,
         name: 'React.js',
-        path: '/assets/react.svg',
+        path: './assets/react.svg',
       },
       {
         id: 2,
         name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        path: './assets/tailwindcss.png',
       },
       {
         id: 3,
         name: 'TypeScript',
-        path: '/assets/typescript.png',
+        path: './assets/typescript.png',
       },
       {
         id: 4,
         name: 'Framer Motion',
-        path: '/assets/framer.png',
+        path: './assets/framer.png',
       },
     ],
   },
@@ -176,34 +176,34 @@ export const myProjects = [
     subdesc:
       'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
     href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
-    texture: '/textures/project/project4.mp4',
-    logo: '/assets/project-logo4.png',
+    texture: './textures/project/project4.mp4',
+    logo: './assets/project-logo4.png',
     logoStyle: {
       backgroundColor: '#0E1F38',
       border: '0.2px solid #0E2D58',
       boxShadow: '0px 0px 60px 0px #2F67B64D',
     },
-    spotlight: '/assets/spotlight4.png',
+    spotlight: './assets/spotlight4.png',
     tags: [
       {
         id: 1,
         name: 'React.js',
-        path: '/assets/react.svg',
+        path: './assets/react.svg',
       },
       {
         id: 2,
         name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        path: './assets/tailwindcss.png',
       },
       {
         id: 3,
         name: 'TypeScript',
-        path: '/assets/typescript.png',
+        path: './assets/typescript.png',
       },
       {
         id: 4,
         name: 'Framer Motion',
-        path: '/assets/framer.png',
+        path: './assets/framer.png',
       },
     ],
   },
@@ -213,34 +213,34 @@ export const myProjects = [
     subdesc:
       'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
     href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    texture: '/textures/project/project5.mp4',
-    logo: '/assets/project-logo5.png',
+    texture: './textures/project/project5.mp4',
+    logo: './assets/project-logo5.png',
     logoStyle: {
       backgroundColor: '#1C1A43',
       border: '0.2px solid #252262',
       boxShadow: '0px 0px 60px 0px #635BFF4D',
     },
-    spotlight: '/assets/spotlight5.png',
+    spotlight: './assets/spotlight5.png',
     tags: [
       {
         id: 1,
         name: 'React.js',
-        path: '/assets/react.svg',
+        path: './assets/react.svg',
       },
       {
         id: 2,
         name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        path: './assets/tailwindcss.png',
       },
       {
         id: 3,
         name: 'TypeScript',
-        path: '/assets/typescript.png',
+        path: './assets/typescript.png',
       },
       {
         id: 4,
         name: 'Framer Motion',
-        path: '/assets/framer.png',
+        path: './assets/framer.png',
       },
     ],
   },
@@ -264,7 +264,7 @@ export const workExperiences = [
     pos: 'Subject Matter Expert',
     duration: '2022 - 2023',
     title: "Provided expert guidance and solutions to student queries in various computer science subjects, including data structures and algorithms, programming language (C++/Python), Object Oriented Programming and more.",
-    icon: '/assets/chegg.svg',
+    icon: './assets/chegg.svg',
     animation: 'victory',
   },
   {
@@ -273,7 +273,7 @@ export const workExperiences = [
     pos: 'Member',
     duration: '2021 - Present',
     title: "My journey as a GDSC member serves as a testament to the power of perseverance, resilience, and unwavering belief in one's dreams. I have helped students in developing their communication skills, management skills, and coding skills.",
-    icon: '/assets/gdsc.svg',
+    icon: './assets/gdsc.svg',
     animation: 'clapping',
   },
   {
@@ -282,7 +282,7 @@ export const workExperiences = [
     pos: 'Junior Web Developer',
     duration: '2024 (May-June)',
     title: "As a Web Developer Intern at CodeAlpha, I gained valuable hands-on experience in applying MERN stack technologies to solve to real-world problems. Created various projects such as Podcastr-AI Platform a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. and many more.",
-    icon: '/assets/codealpha.png',
+    icon: './assets/codealpha.png',
     animation: 'salute',
   },
 ];
