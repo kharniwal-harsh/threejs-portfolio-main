059035df9e77ba933133ac13ab144a3cd5830ffa
harsh kharniwal <115343128+kharniwal-harsh@users.noreply.github.com>
2024-11-24 14:49:33 +0530

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -95,7 +95,7 @@ const About = () => {
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">harshkharniwal9351@gmail.com</p>
+                <p className="lg:text-2xl md:text-2xl font-medium text-gray_gradient text-white">harshkharniwal9351@gmail.com</p>
               </div>
             </div>
           </div>
