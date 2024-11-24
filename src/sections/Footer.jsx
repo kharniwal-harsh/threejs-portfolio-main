20dbc4f8bce3a8474fdc98418c94f6b052c7fe78
harsh kharniwal <115343128+kharniwal-harsh@users.noreply.github.com>
2024-11-24 14:55:20 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,23 +7,28 @@ const Footer = () => {
         <p>Privacy Policy</p>
       </div>
 
-      <div className="flex gap-3">
-
-      
+{/*       <div className="flex gap-3">
   <div className="social-icon" onClick={() => window.open("https://github.com/kharniwal-harsh", "_blank")}>
     <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
   </div>
-
-
-
         <div className="social-icon" onClick={() => window.open("https://www.linkedin.com/in/harsh-kharniwal9351/", "_blank")}>
           <img src="/assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2"  />
         </div>
-
         <div className="social-icon" onClick={() => window.open("https://github.com/your-username", "_blank")}>
            <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
         </div>
+      </div> */}
 
+      <div className="flex gap-3">
+        <div className="social-icon">
+          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+        </div>
+        <div className="social-icon">
+          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+        </div>
+        <div className="social-icon">
+          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+        </div>
       </div>
 
       <p className="text-white-500">© 2024 Harsh Kharniwal. All rights reserved.</p>
